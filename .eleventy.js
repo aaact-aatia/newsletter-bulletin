192d187aa7d87a8f72258424e10e9093c8a5427e
Shawn Thompson <plansmash@me.com>
2025-02-19 12:01:51 -0500

    add Eleventy configuration to integrate with main site

---
+++ .eleventy.js
@@ -0,0 +1,18 @@
+const mainConfig = require("./main-site/eleventy.config.js"); // Import the main site config
+
+module.exports = function (eleventyConfig) {
+	// Load everything from the main site's config
+	const config = mainConfig(eleventyConfig);
+
+	// Override the directories specific to this project
+	return {
+		...config, // Spread operator to merge main site's config
+		dir: {
+			input: "content",
+			data: "../main-site/_data",
+			includes: "../main-site/_includes",
+			layouts: "../main-site/_includes/layouts",
+			output: "_site"
+		}
+	};
+};
